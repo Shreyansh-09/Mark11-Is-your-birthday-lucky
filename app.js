f04f9a252bc6f508c24d54697deac4800e5a1e6a
Shreyansh-09 <2509shreyansh@gmail.com>
2022-09-07 21:32:25 +0530

    Mark11-done
    Mark 11 - Is your Birthday Lucky? is done

---
+++ app.js
@@ -0,0 +1,35 @@
+var dateofBirth = document.querySelector("#date-of-birth")
+var luckyNumber = document.querySelector("#lucky-number")
+var btnCheck = document.querySelector("#btn-check")
+var outputDiv = document.querySelector("#output-div")
+
+
+function isBirthdayLucky(sum, luckyno){
+    if(sum % luckyno === 0){
+        outputDiv.innerText = "your Birthday is lucky!!!!🚀🚀"
+    }
+    else{
+        outputDiv.innerText = "Your Birthday is not Lucky...🙁🙁"
+    }
+}
+
+
+function calculateSum(dob){
+    var sum = 0;
+    for(var idx = 0; idx < dob.length; idx++){
+        var num = dob.charAt(idx);
+        sum = sum + Number(num)
+    }
+    return(sum)
+}
+
+
+function clickEventHandler() {
+    var dateValue = dateofBirth.value;
+    var dob = dateValue.replaceAll("-","")
+    var sumOfDob = calculateSum(dob)
+    var result = isBirthdayLucky(sumOfDob, luckyNumber.value)
+}
+
+
+btnCheck.addEventListener("click", clickEventHandler)
